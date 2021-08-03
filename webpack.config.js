87eab833f3c27daf3125f08eae87ce701aba967c
李珅 <likun@xjzhicheng.cn>
2021-08-03 13:49:46 +0800

    babel配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,21 @@
 module.exports = {
   mode: "development",
-  entry: "./src/index.js"
+  entry: "./src/index.js",
+  module: {
+    rules: [{
+      test: /\.js$/,
+      use: [{
+        loader: "babel-loader",
+        options: {
+          presets: [[
+            '@babel/preset-env', {
+              targets: '> 1%',
+              useBuiltIns: "usage",
+              corejs: 3
+            }
+          ]]
+        }
+      }]
+    }]
+  }
 }
